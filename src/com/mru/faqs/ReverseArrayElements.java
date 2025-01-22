abf1841f287530d022d92054429c732ad87bd27e
Venkat5002 <venigallavenkatarupin@gmail.com>
2025-01-22 11:24:54 +0530

    i hate my life

---
+++ src/com/mru/faqs/ReverseArrayElements.java
@@ -0,0 +1,26 @@
+package com.mru.faqs;
+
+import java.util.*;
+
+
+public class ReverseArrayElements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int[] arr = {10,20,30,40,50};
+		
+		int start = 0;
+		int end = arr.length-1;
+		
+		while (start<end) {
+			int temp = arr[start];
+			arr[start] = arr[end];
+			arr[end] = temp;
+			start++;
+			end--;
+		}
+		System.out.println(Arrays.toString(arr));
+
+	}
+
+}
